c2cd5d1279ece55bf5b6837db5eddb6332028fd6
Alona Flores <79310481+AlonaFlores25@users.noreply.github.com>
2021-04-15 20:08:10 +0800

    Delete Project_g8_Brgy-eVaccination directory

--- Project_g8_Brgy-eVaccination/BrgyEVaccination/app/src/main/java/com/example/brgye_vaccination/Config.java
+++
@@ -1,9 +0,0 @@
-package com.example.brgye_vaccination;
-
-public class Config {
-    public static final String DATA_URL = "https://recursive-menu.000webhostapp.com/Datalist.php";
-
-    public static final String KEY_NAME = "parent_name";
-
-    public static final String JSON_ARRAY = "result";
-}
